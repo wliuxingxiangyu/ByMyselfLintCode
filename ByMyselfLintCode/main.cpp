a86728b8bd5fe6d4f41dff3cd837e4da4e964cb4
wliuxingxiangyu <wliuxingxiangyu@163.com>
2016-05-01 23:55:29 +0800

    1减去string末尾多余的字符串，“不能”将末尾变成\0.就以为OK，

--- ByMyselfLintCode/main.cpp
+++ ByMyselfLintCode/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <string.h>
+#include <sstream> 
+#include <stack> 
 using namespace std;
 class TreeNode {
 public:
@@ -12,35 +14,52 @@ public:
 		this->left = this->right = NULL;
 	}
 };
-
-void process(TreeNode* root,vector<char> vc,vector<string>&  v){
+//string int2String(int a){
+//	stack<int> s;
+//	while (a){ 
+//		s.push(a%10);
+//		a=a/10;
+//	}
+//    char str[ s.size() ];
+//	while (s.size() ){ 
+//		cout<< 
+//		str[]= s.top();
+//		s.pop();
+//	}
+//	return str;
+//}
+string int2str(const int &int_temp)  {  
+	stringstream stream;  
+	stream<<int_temp;  
+	string string_temp;
+	stream>>string_temp ;   //此处也可以用 stream>>string_temp  
+	return string_temp;
+}
+void process(TreeNode* root,vector<int> vc,vector<string>&  v){
 	if (root != NULL && root->left == NULL && root->right == NULL) {
-		vc.push_back(root->val + '0');
+		vc.push_back(root->val);
 		string str="";
-		for (vector<char>::iterator it=vc.begin();it!=vc.end();it++){ 
-			str += *it;
-			// str += "->";
+		vector<int>::iterator it;
+		for (it=vc.begin();it!=vc.end();it++){ 
+			str += int2str(*it);
+			// str +=itoa((*it),strTemp,10);//itoa函数int-->string有些IDE不识别。	
+			str += "->";
 		}
+		int len=str.size();
+		str [len-2]='\0';
+		cout<<str<<endl;
 		v.push_back(str);
 		return ; 
 	}
 
-	vc.push_back(root->val + '0');//int 10+48_'0'=58_':'
+	vc.push_back(root->val );//int 10+48_'0'=58_':'
 
 	if(root->left != NULL){
-		vc.push_back('-');
-		vc.push_back('>');
 		process(root->left,vc ,v);
-		vc.pop_back();
-		vc.pop_back();
 	}
 
 	if(root->right != NULL){
-		vc.push_back('-');
-		vc.push_back('>');
 		process(root->right,vc,v);
-		vc.pop_back();
-		vc.pop_back();
 	}
 
 	//从vc中删除尾字符，往上回溯。进入下一个分支。
@@ -50,18 +69,18 @@ void process(TreeNode* root,vector<char> vc,vector<string>&  v){
 vector<string> binaryTreePaths(TreeNode* root) {
 	vector<string>  v;
 	if (root == NULL) return v;
-	vector<char> vc;
+	vector<int> vc;
 	process(root,vc,v);
 	return v;
 }
 
 int  main(){
-	TreeNode* root=new TreeNode(10);
-	//root->left=new TreeNode(2);
-	//root->right=new TreeNode(3);
+	TreeNode* root=new TreeNode(1);
+	root->left=new TreeNode(2);
+	root->right=new TreeNode(3);
 
-	//root->left->left=NULL;
-	//root->left->right=new TreeNode(5);
+	root->left->left=NULL;
+	root->left->right=new TreeNode(5);
 
 	binaryTreePaths(root);
 	system("pause");
