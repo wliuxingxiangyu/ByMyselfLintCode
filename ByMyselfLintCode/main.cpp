27a9fa4be3c4378ef325ac259e6b16bfb85a616b
wliuxingxiangyu <wliuxingxiangyu@163.com>
2016-05-10 10:38:19 +0800

    http://www.lintcode.com/zh-cn/problem/valid-palindrome/ 字符串边界处理。 给定一个字符串，判断其是否为一个回文串。只包含字母和数字，忽略大小写。
     注意事项
    
    你是否考虑过，字符串有可能是空字符串？这是面试过程中，面试官常常会问的问题。
    
    在这个题目中，我们将空字符串判定为有效回文。
    
    您在真实的面试中是否遇到过这个题？ Yes
    样例
    "A man, a plan, a canal: Panama" 是一个回文。
    
    "race a car" 不是一个回文。

--- ByMyselfLintCode/main.cpp
+++ ByMyselfLintCode/main.cpp
@@ -10,26 +10,42 @@
 #include <queue> 
 using namespace std;
 
-int processInter(int n){
-	int arr[100]={0};
-	arr[1]=1;arr[2]=2;
-	for (int i=3;i <= n;i++) //台阶第一步为1种，第二步为2种，所以n取等号.
-	{ arr[i]=arr[i-1]+arr[i-2];
+bool isCharOrNumber(char c){
+	if ( (c>='0' && c<='9') || (c>='A' && c<='Z') || (c>='a' && c<='z') ){ 
+		return true;
+	} else{
+		return false;
 	}
-	return arr[n];
 }
 
-int array[100]={0};//递归版 全局数组
-int processRecur(int n){
-	if (n == 1) return array[1]=1;//先将值赋给数组，再返回。
-	if (n == 2) return array[2]=2;
-	if (array[n] != 0) return array[n];
-	return array[n]=processRecur( n-1)+processRecur( n-2);
+bool isPalindrome(string& s) {
+	int len=s.size();
+	if( len == 0) return true;
+	int i=0,j=len-1;
+	char c1,c2;
+	while(i<j){
+		while(i<len && ! isCharOrNumber(s[i]) ) i++;
+		if(i==len) return true; //全是非字符，返回true.
+
+		if(s[i]>='A' && s[i]<='Z'  ) c1=s[i]+32;
+		else c1=s[i];
+
+		while(j>0 && ! isCharOrNumber(s[j]) )  j--;
+
+		if(s[j]>='A' && s[j]<='Z'  ) c2=s[j]+32;
+		else c2=s[j];
+
+		if(c1 != c2) return false;
+		i++;j--;
+	}
+
+	return true;
+
 }
 
 int  main(){
-	int maxStep=10;
-	cout<< "迭代版processInter(10)="<< processInter(maxStep)<<endl;
-	cout<< "递归版processRecur(10)="<<processRecur(maxStep) <<endl;
+	string str="1a2";
+	cout<< "isPalindrome(str)="<< isPalindrome(str)<<endl;
+
 	system("pause");
 }
