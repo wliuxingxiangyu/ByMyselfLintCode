acbeda8f6aaecefbdb408abc7664f31bec1f271f
wliuxingxiangyu <wliuxingxiangyu@163.com>
2017-02-17 00:57:43 +0800

    快排

--- ByMyselfLintCode/main.cpp
+++ ByMyselfLintCode/main.cpp
@@ -10,54 +10,43 @@
 #include <queue> 
 using namespace std;
 
-int pow(int x,int y){
-	if (y == 0) return 1;
+int getMidIndex(vector<int> &nums,int low,int high){
+	int tmp=nums[low];
+	while(low<high){
+		while(low<high&&nums[high]>=tmp){
+			high--;
+		}
+		nums[low]=nums[high];
 
-	while(--y){
-		x *=10;
+		while(low<high&&nums[low]<tmp){
+			low++;
+		}
+		nums[high]=nums[low];
 	}
-	return x;
+	nums[low]=tmp;// 中轴元素归位,
+	return low; // 返回中轴的位置
 }
-//判断1221是否是回文数？myself思路乱。两个位数往中间走，不如lintcode直接操作原数。
-bool isPalindromeMy(int x) {//代码有误
-	if (x<10 && x>-10)  return true;//-12321为回文数，先取绝对值
-	int temp=abs(x),itNum=0;//迭代次数 为了mod出 最低位 
-	int power=1,high =temp/10,low,shang;
-	while (high>10) { high /=10; power++;}
 
-	while (itNum <= (power+1)/2 ){
-	    shang=temp/pow(10,itNum);
-		low=shang %10;
-		cout<<"itNum="<<itNum<<",(power+1)/2="<<(power+1)/2  <<",high="<<high<<",low="<<low<<endl;
-		if ( high != low)  return false;
-		
-		itNum++;
-		high=temp%pow(10, power)/pow(10, power-1);
-		power--;
+void quickSort(vector<int> &nums,int low,int high){
+	if(low<high){
+		int mid=getMidIndex(nums,low,high); 
+		quickSort(nums,low,mid);
+		quickSort(nums,mid+1,high);
 	}
-	return true;
-}
-//lintcode参考代码
-bool isPalindrome(int x) {
-	if (x < 0) return false;
-	int d = 1; // divisor
-	while (x / d >= 10) d *= 10; //1234的d=1000.
-	while (x > 0) {
-		int q = x / d; // quotient
-		int r = x % 10; // remainder
-		if (q != r) return false;
-		x = x % d / 10; //去除x的最前最后一位。模最高幂次，再除10.
-		d /= 100;
-	}
-	return true;
+} 
+
+int median(vector<int> &nums) {
+	int len=nums.size();
+	if(len==0) return 0;
+	quickSort(nums,0,len-1);
+	return nums[(len-1)/2];
 }
 
 
 int  main(){
-	//int i=1234674321;
-	int i=123721;
-	cout<< "pow(10,2)="<< pow(10,2)<<endl;
-	cout<< "isPalindrome(str)="<< isPalindrome(i)<<endl;
-	//cout<< "isPalindrome(str)="<< isPalindromeMy(i)<<endl;
+	int arr[]={4,5,1,2,3};
+	int num=sizeof(arr)/sizeof(arr[0]);
+	vector<int> nums(arr,arr+num);
+	cout<< "median(nums)="<< median(nums)<<endl;
 	system("pause");
 }
